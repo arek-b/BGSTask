c70225a2d0342bcfe754f50bc6cc8a8ab2da1e6c
Arkadiusz Bergmann <arkadiuszbergmann@gmail.com>
2024-05-04 14:30:00 +0200

    UI canvas integer scaling

---
+++ Assets/Scripts/UI/CanvasIntegerScaling.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    [ExecuteAlways]
+    public class CanvasIntegerScaling : MonoBehaviour
+    {
+        [SerializeField] private int minimumScreenWidthPixels = 960;
+        [SerializeField] private int minimumScreenHeightPixels = 540;
+        [SerializeField] private CanvasScaler scaler;
+
+        private void OnValidate()
+        {
+            if (scaler == null)
+            {
+                TryGetComponent<CanvasScaler>(out scaler);
+            }
+        }
+
+        private void OnRectTransformDimensionsChange()
+        {
+            float widthScaleFactor = Mathf.Max(Mathf.Floor(Screen.width / minimumScreenWidthPixels), 1f);
+            float heightScaleFactor = Mathf.Max(Mathf.Floor(Screen.height / minimumScreenHeightPixels), 1f);
+            scaler.scaleFactor = Mathf.Min(widthScaleFactor, heightScaleFactor);
+        }
+    }
+}
